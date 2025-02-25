9bf5059988962d2893f36e4b8b339d0916c8c53a
DewitaA12 <xsclthzn@gmail.com>
2025-02-26 01:18:23 +0700

    Menambahkan Parameter Route

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,17 @@ Route::get ('/welcome', function () {
 Route::get ('/about', function () {
     return 'NIM 2341720100 <br> Dewita Anggraini';
 });
+
+//Parameter Route
+
+Route::get ('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get ('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke- '.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get ('/articles/{id}', function ($Id) {
+    return 'Halaman artikel dengan ID '.$Id;
+});
