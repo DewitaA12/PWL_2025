343edfb4db0e688aa19b1d15a0a3124d954422ea
DewitaA12 <xsclthzn@gmail.com>
2025-02-26 01:13:33 +0700

    Menambahkan Basic Route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Basic Route
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get ('/world', function () {
+    return 'World';
+});
+
+Route::get ('/welcome', function () {
+    return 'Selamat Datang';
+});
+
+Route::get ('/about', function () {
+    return 'NIM 2341720100 <br> Dewita Anggraini';
+});
